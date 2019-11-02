fdac518debc6243950e3182197a0d5be070dbffb
wismannur <wismannura@gmail.com>
2019-11-02 23:25:12 +0700

    fix style section activity and videos

--- src/app/section-activity/section-activity.page.ts
+++ src/app/section-activity/section-activity.page.ts
@@ -7,6 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SectionActivityPage implements OnInit {
 
+  datalist: any = [
+    {
+      id: 0,
+      class: 'not-active',
+    },
+    {
+      id: 1,
+      class: 'not-active',
+    },
+    {
+      id: 2,
+      class: 'active',
+    },
+    {
+      id: 3,
+      class: 'not-active',
+    },
+    {
+      id: 4,
+      class: 'not-active',
+    },
+    {
+      id: 5,
+      class: 'not-active',
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
