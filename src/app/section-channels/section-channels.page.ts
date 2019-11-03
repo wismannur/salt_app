3475f0bb6f8b96d54a16546ddc7a053737f1c839
wismannur <wismannura@gmail.com>
2019-11-03 13:30:45 +0700

    fix style section activity and channels

--- src/app/section-channels/section-channels.page.ts
+++ src/app/section-channels/section-channels.page.ts
@@ -11,42 +11,52 @@ export class SectionChannelsPage implements OnInit {
     {
       id: 0,
       class: 'not-active',
+      name: 'Google',
     },
     {
       id: 1,
-      class: 'not-active',
+      class: 'active',
+      name:  'Dribble',
     },
     {
       id: 2,
-      class: 'active',
+      class: 'not-active',
+      name:  'Dribble',
     },
     {
       id: 3,
       class: 'not-active',
+      name:  'Dribble',
     },
     {
       id: 4,
       class: 'not-active',
+      name:  'Dribble',
     },
     {
       id: 5,
       class: 'not-active',
+      name:  'Dribble',
     },
     {
       id: 6,
       class: 'not-active',
+      name:  'Dribble',
     },
     {
       id: 7,
       class: 'not-active',
+      name:  'Dribble',
     },
     {
       id: 8,
       class: 'not-active',
+      name:  'Dribble',
     },
     {
       id: 9,
       class: 'not-active',
+      name:  'Dribble',
     },
   ]
 
